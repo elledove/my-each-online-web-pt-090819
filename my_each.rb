61e6d1d896c042e96e191cd31c9b3f2cb6fdd701
Lauren Dove <elledoveinc@gmail.com>
2019-09-21 17:23:56 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,11 @@
-def my_each # put argument(s) here
-  # code here
+array = ["mommy" , "daddy" "bro"]
+def my_each(array)
+      e = 0 
+    while e < array.length
+     (array) do |confused|
+     return confused
+    end
+    end # put argument(s) here
+                     # code here
 end
+
